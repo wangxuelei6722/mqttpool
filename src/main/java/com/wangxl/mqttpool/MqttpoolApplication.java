b33fdb2795c125a96c20783ae80acb9209e04b03
wangxl <475524154@qq.com>
2022-03-03 14:20:39 +0800

    初始化

---
+++ src/main/java/com/wangxl/mqttpool/MqttpoolApplication.java
@@ -0,0 +1,13 @@
+package com.wangxl.mqttpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttpoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttpoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangxl/mqttpool/MqttpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangxl.mqttpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttpoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
